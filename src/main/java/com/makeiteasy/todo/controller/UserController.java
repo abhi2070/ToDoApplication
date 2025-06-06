0e2a2b6792f52c0cac745039d752fab4642b7219
abhi2070 <Abhijeetsingh2070@gmail.com>
2025-06-06 20:18:14 +0530

    Post and Delete API

--- src/main/java/com/makeiteasy/todo/controller/UserController.java
+++ src/main/java/com/makeiteasy/todo/controller/UserController.java
@@ -3,7 +3,10 @@ package com.makeiteasy.todo.controller;
 import com.makeiteasy.todo.model.User;
 import com.makeiteasy.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,8 +20,25 @@ public class UserController {
        return userService.getUserDetailById(id);
    }
 
+   @GetMapping("/getUsers")
+   public List<User> getUsers(){
+       List<User> users=userService.getAllUsers();
+       return  users;
+   }
+
     @PostMapping("/newUser")
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public User updateUser(@PathVariable long id, @RequestBody User user){
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/removeUser/{id}")
+    public ResponseEntity<String>  deleteUser(@PathVariable long id){
+       userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
     }
 }
--- src/main/java/com/makeiteasy/todo/service/UserService.java
+++ src/main/java/com/makeiteasy/todo/service/UserService.java
@@ -5,6 +5,8 @@ import com.makeiteasy.todo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -14,7 +16,25 @@ public class UserService {
         return userRepository.findById(id).get();
     }
 
+    public List<User> getAllUsers(){
+        List<User> users=userRepository.findAll();
+        return  users;
+    }
+
     public User createUser(User user){
         return userRepository.save(user);
+    }
+
+    public User updateUser(long id, User user){
+        User existingUser = userRepository.findById(id).get();
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(long id){
+        User existingUser = userRepository.findById(id).get();
+        userRepository.delete(existingUser);
     }
 }
